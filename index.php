0eb366170a74ab9366b0ccf206778bf2ea3e2d4c
Juan Daniel <danielalejandre1050@gmail.com>
2022-03-22 01:23:19 -0600

    Index.php finished

--- index.php
+++ index.php
@@ -1,5 +1,70 @@
 <?php include("./includes/header.php") ?>
     <div class="container">
-        <h1>MYSSD PIA</h1>
+        <h1>MYSSD</h1>
+        <div class="wrapper">
+            <div class="card">
+                <div class="content">
+                    <h2>01</h2>
+                    <h3>Generador Congruencial Mixto</h3>
+                    <a href="#">Probar simulación</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <h2>02</h2>
+                    <h3>Generador Congruencial Multiplicativo</h3>
+                    <a href="#">Probar simulación</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <h2>03</h2>
+                    <h3>Prueba Estadística de Promedio</h3>
+                    <a href="#">Probar simulación</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <h2>04</h2>
+                    <h3>Prueba Estadística de Kolmogorov-Smirnov</h3>
+                    <a href="Kolmogorov-Smirnov/kolmogorov.php">Probar simulación</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <h2>05</h2>
+                    <h3>Prueba Estadística de Frecuencias</h3>
+                    <a href="#">Probar simulación</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <h2>06</h2>
+                    <h3>Prueba Estadística de Series</h3>
+                    <a href="#">Probar simulación</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <h2>07</h2>
+                    <h3>Distribución Exponencial</h3>
+                    <a href="#">Probar simulación</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <h2>08</h2>
+                    <h3>Distribución Uniforme</h3>
+                    <a href="#">Probar simulación</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <h2>09</h2>
+                    <h3>Distribución de Poisson</h3>
+                    <a href="#">Probar simulación</a>
+                </div>
+            </div>
+        </div>
     </div>
 <?php include("./includes/footer.php") ?>
