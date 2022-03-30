ba72884006b9eaf4512f87ec2685166bc24a367c
Juan Daniel <danielalejandre1050@gmail.com>
2022-03-30 15:04:42 -0600

    Kolmogorov doesnt append and store the elements into an array

--- simulations/kolmogorov_manual.php
+++ simulations/kolmogorov_manual.php
@@ -27,25 +27,40 @@ function get_input($data)
 
 <main class="form-main">
     <div class="form-wrapper result-wrapper">
-        <a href="../simulations/kolmogorov.php"><h4>Prueba Estadístico de Kolmogorov-Smirnov aleatorios</h4></a>
+        <a href="../simulations/kolmogorov.php">
+            <h4>Prueba Estadístico de Kolmogorov-Smirnov aleatorios</h4>
+        </a>
     </div>
     <div class="form-wrapper">
-    <div class="form-container">
-        <form class="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-            <div class="form-line">
-                <label for="num_rectangulares">Cantidad de números rectangulares</label>
-                <input type="number" id="num" name="num_rectangulares" value="<?php echo $num_rectangulares; ?>" placeholder="Valor en enteros" required>
-            </div>
-            <div class="form-line">
-                <label for="alfa">Valor porcentual de alfa (α)</label>
-                <input type="number" id="alfa" name="alfa" value="<?php echo $alfa; ?>" placeholder="Valor de α" required>
-            </div>
-            <div class="form-button">
-                <input class="button" type="submit" name="submit" value="Calcular">
-            </div>
-        </form>
+        <div class="form-container">
+            <form class="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                <div class="form-line">
+                    <label for="num_rectangulares">Números rectangulares</label>
+                    <input type="number" id="num" name="num_rectangulares" value="<?php echo $num_rectangulares; ?>" placeholder="Valor en enteros" required>
+                </div>
+                <div class="form-button">
+                    <input class="add-button" type="submit" name="add" value="Agregar" onclick="addNumber()">
+                </div>
+                <div class="form-line">
+                    <label for="alfa">Valor porcentual de alfa (α)</label>
+                    <input type="number" id="alfa" name="alfa" value="<?php echo $alfa; ?>" placeholder="Valor de α" required>
+                </div>
+                <div class="form-button">
+                    <input class="button" type="submit" name="submit" value="Calcular">
+                </div>
+            </form>
+        </div>
     </div>
-    </div>
+    <?php
+    $number_list = [];
+        if (isset($_POST['add'])){
+            array_push($number_list, $num_rectangulares);
+            foreach ($number_list as $num) {
+                echo $num . "<br>"; 
+             }
+             print_r($number_list);
+        }
+    ?>
 </main>
 
 <?php include("../includes/footer.php") ?>
